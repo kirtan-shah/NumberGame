5e9b2c0699178c60b78fe41a7d3169db9f95606d
kirtan-shah <kirtanushah@gmail.com>
2018-06-27 16:54:34 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,39 @@
 
 
+let x = 0;
+let y = 0;
+
+let up = false, down = false, left = false, right = false;
+
 function setup() {
   createCanvas(960, 540);
+  x = width/2;
+  y = height/2;
 }
 
-function draw() {
+function keyDown() {
+  if(keyCode == UP) up = true;
+  if(keyCode == DOWN) down = true;
+  if(keyCode == LEFT) left = true;
+  if(keyCode == RIGHT) right = true;
+}
+
+function keyUp() {
+  if(keyCode == UP) up = false;
+  if(keyCode == DOWN) down = false;
+  if(keyCode == LEFT) left = false;
+  if(keyCode == RIGHT) right = false;
+}
+
+var speed = 5;
+
+function draw() { 
+  if(up) y -= speed;
+  if(down) y += speed;
+  if(left) x -= speed;
+  if(right) x += speed;
+  
   background(0);
   fill(255);
-  ellipse(mouseX, mouseY, 20, 20);
+  ellipse(x, y, 20, 20);
 }
